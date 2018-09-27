938e6a62585e8616a71432536f50408a3a33085c
Swatantra Gupta <SwatantraGupta-1402@users.noreply.github.com>
2018-09-27 12:58:43 +0530

    Delete File

--- index_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo "Welcome Swatantra Gupta in Git Hub Acccount.";
-	echo "Hello!"; 
-?>
-<?php
-	echo "This account has been created just for testing purpose.";
-	echo "Please watch full video and like it.";
- ?>
